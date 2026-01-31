62a9d324fcd19eb70d820ac8bb4ba56b937a9964
0xleft <admin@0xleft.com>
2026-01-31 13:40:27 +0100

    Initial structure

---
+++ src/core/gui/window_manager.cpp
@@ -0,0 +1,15 @@
+//
+// Created by adomas on 31-01-2026.
+//
+
+#include "window_manager.h"
+
+namespace imshark
+{
+    namespace core
+    {
+        namespace gui
+        {
+        } // gui
+    } // core
+} // imshark
---
+++ src/core/gui/window_manager.h
@@ -0,0 +1,21 @@
+//
+// Created by adomas on 31-01-2026.
+//
+
+#ifndef IMSHARK_WINDOW_MANAGER_H
+#define IMSHARK_WINDOW_MANAGER_H
+
+namespace imshark
+{
+    namespace core
+    {
+        namespace gui
+        {
+            class window_manager
+            {
+            };
+        } // gui
+    } // core
+} // imshark
+
+#endif //IMSHARK_WINDOW_MANAGER_H
